27e7d9871fc3db93ad6c492972c20317c89b4be5
rfoleyiq <rfoley@iqsolutions.com>
2016-03-26 08:16:34 -0400

    fixed and cleaning up stuff

--- templates/feed-output.js
+++
@@ -1,7 +0,0 @@
-{{nodes}}
-    <div class="divwrap">
-        <h2><a href="{{path}}">{{title}}</a></h2>
-        <a href="{{path}}"><img src="{{field_stats_image_1.src}}" alt="{{field_stats_image_1.alt}}" /></a>
-        <p>{{field_teaser}} <a href="{{path}}">Read more &raquo;</a></p>
-    </div>
-{{/nodes}}
